c350f737c867dceb0e60a5bc81f7b139073fad7e
dmt-fraczkiewicz <dmt.szymon@lo5.bielsko.pl>
2023-04-24 21:25:51 +0200

    1
    1

--- Problem solving/FairRations.java
+++ Problem solving/FairRations.java
@@ -1,11 +1,25 @@
-import java.util.List;
+import java.util.*;
 
-public class FairRations {
+class Result {
+
+    /*
+     * Complete the 'fairRations' function below.
+     *
+     * The function is expected to return a STRING.
+     * The function accepts INTEGER_ARRAY B as parameter.
+     */
+
     public static String fairRations(List<Integer> B) {
-        // Write your code here
-        // ! every person got even number of loaves
-        // ! if i  then i + and i - 1 
-        
-        return "a";
+    int result = 0;
+    
+    for (int i = 0; i < B.size() ; i++){
+        if (B.get(i)%2 != 0){
+            if (i == B.size()-1) return "NO";
+            B.set(i, B.get(i)+1);
+            B.set(i+1, B.get(i+1)+1);
+            result += 2;
+        }
+    }
+    return Integer.toString(result);        
     }
 }
