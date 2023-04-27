a63b8d13eb286bc3d3bc215bf0ba89169fda2942
dmt-fraczkiewicz <dmt.szymon@lo5.bielsko.pl>
2023-04-27 10:44:24 +0200

    hc

---
+++ Problem solving/BetweenTwoSets.java
@@ -0,0 +1,8 @@
+import java.util.List;
+
+public class BetweenTwoSets {
+    public static int getTotalX(List<Integer> a, List<Integer> b) {
+        
+        return 2;    
+    }
+}
--- Problem solving/NumberLineJumps.java
+++ Problem solving/NumberLineJumps.java
@@ -1,6 +1,22 @@
+// The first kangaroo starts at location x1 and moves at a rate of v1 meters per jump.
+// The second kangaroo starts at location x2 and moves at a rate of v2 meters per jump.
+
 public class NumberLineJumps {
     public static String kangaroo(int x1, int v1, int x2, int v2) {
+        String result = "NO";
+        ;;
         
-        return "a";
+        while(x1 < x2) {
+            x1 += v1;
+            x2 += v2;
+            if(x1 == x2) {
+                result = "YES";
+                return result;
+            }
+        }
+        return result;
+    }
+    public static void main(String[] args) {
+        kangaroo(14, 4, 98, 2);
     }
 }
