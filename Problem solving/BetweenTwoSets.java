b0597609a7a104086eb6d3aa45e7fc4687e3c72b
dmt-fraczkiewicz <dmt.szymon@lo5.bielsko.pl>
2023-04-27 23:24:17 +0200

    hc

--- Problem solving/BetweenTwoSets.java
+++ Problem solving/BetweenTwoSets.java
@@ -5,7 +5,22 @@ import java.util.List;
 
 public class BetweenTwoSets {
     public static int getTotalX(List<Integer> a, List<Integer> b) {
-        
-        return 2;    
+        a.sort(null);
+        b.sort(null);
+        Integer count = 0;
+        for(int i = a.get(a.size() - 1); i <= b.get(0); i ++) {
+            int sum = 0;
+            for(int a_i = 0; a_i < a.size(); a_i ++) {
+                sum += i % a.get(a_i);
+            }
+            for(int b_i = 0; b_i < b.size(); b_i ++) {
+                sum += b.get(b_i) % i;
+            }
+
+            if(sum == 0) {
+                ++ count;
+            }
+        }
+        return count;
     }
 }
---
+++ Problem solving/BreakingTheRecords.java
@@ -0,0 +1,7 @@
+import java.util.List;
+
+public class BreakingTheRecords {
+    public static List<Integer> breakingRecords(List<Integer> scores) {
+        
+    }
+}
